bf4421a79170aea164a05405cfbe9d13199775d5
Juan Elvir <jelvir97@gmail.com>
2024-08-23 22:09:17 -0600

    deploy fixes #4

--- art_quiz/settings.py
+++ art_quiz/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import environ
 
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mfuravdd!49-8n=nja)jngs8k3&!(z#kas0$80(2(x(^em9uxc'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -123,10 +127,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Initialise environment variables
-env = environ.Env()
-environ.Env.read_env()
 
 # OPENAI_KEY
 OPENAI_KEY = env('OPENAI_KEY')
